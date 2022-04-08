f084d4ab02a6288172cb9c0c7071241671e29f5d
Marks Polakovs <marks@markspolakovs.me>
2022-04-08 19:50:12 +0100

    GRAPHICS-21: Initial implementation of netball
    Front-end and back-end could both do some work, and when we implement a third sport we'll likely want to refactor a lot of the core logic into functions.

--- scores-src/src/client/eventTypes.tsx
+++ scores-src/src/client/eventTypes.tsx
@@ -1,6 +1,8 @@
 import { typeInfo as football } from "../common/sports/football";
+import { typeInfo as netball } from "../common/sports/netball";
 import { EventTypeInfo } from "../common/types";
 
 export const EVENTS: Record<string, EventTypeInfo<any>> = {
   football,
+  netball,
 };
