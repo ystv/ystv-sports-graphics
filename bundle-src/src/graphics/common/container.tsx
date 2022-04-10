522a812c664ce8cfdaaae64b6fe238904bbd4dd6
Marks Polakovs <marks@markspolakovs.me>
2022-04-10 08:38:45 +0100

    GRAPHICS-138: Bundle: dashboard/graphics setup for adding sports
    This change is mainly to keep sport-specific changes separate from the "core" changes to minimise merge conflicts.

---
+++ bundle-src/src/graphics/common/container.tsx
@@ -0,0 +1,16 @@
+export function GraphicContainer({
+  zIndex = 0,
+  children,
+}: {
+  zIndex?: number;
+  children: JSX.Element | JSX.Element[];
+}) {
+  const styles: React.CSSProperties = {
+    position: "absolute",
+    top: 0,
+    height: "100%",
+    width: "100%",
+    zIndex: zIndex,
+  };
+  return <div style={styles}>{children}</div>;
+}
