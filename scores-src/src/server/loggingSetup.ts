c9c4c2fe01b47b46c009b4fd19b70eaa35947604
Marks Polakovs <marks@markspolakovs.me>
2022-04-10 17:22:20 +0100

    Don't write our own logfmt stringifier

--- scores-src/src/server/loggingSetup.ts
+++ scores-src/src/server/loggingSetup.ts
@@ -1,31 +1,26 @@
 import config from "./config";
 import winston from "winston";
+import { stringify } from "logfmt";
 
-const whitespace = /\s/;
-const makeLogfmt = (properties: Record<string, unknown>, escapeJSON = true) =>
-  Object.keys(properties)
-    .map((prop) => {
-      const value = properties[prop];
-      const valueStr =
-        typeof value === "string" ? value : JSON.stringify(value);
-      const valueFmt =
-        whitespace.test(valueStr) || valueStr.length === 0
-          ? `"${escapeJSON ? valueStr.replaceAll(`"`, `\\"`) : valueStr}"`
-          : value;
-      return `${prop}=${valueFmt}`;
-    })
-    .join(" ");
+function logfmt(data: Record<string, unknown>) {
+  for (const key of Object.keys(data)) {
+    if (typeof data[key] !== "string") {
+      data[key] = JSON.stringify(data[key]);
+    }
+  }
+  return stringify(data);
+}
 
 const format = winston.format.combine(
   winston.format.timestamp(),
   process.env.NODE_ENV === "production"
     ? winston.format.printf(({ level, message, timestamp, name, ...meta }) =>
-        makeLogfmt({ timestamp, level, name, message, ...meta })
+        logfmt({ timestamp, level, name, message, ...meta })
       )
     : winston.format.printf(({ level, message, timestamp, name, ...rest }) => {
         let restStr = "";
         if (Object.keys(rest).length > 0) {
-          restStr = `\n\t ${makeLogfmt(rest, false)}`;
+          restStr = `\n\t ${logfmt(rest)}`;
         }
         return `${timestamp} ${level} ${name}: ${message}${restStr}`;
       })
