b961df39fc17cfdbcb464c0c51f1b74374ca5483
Marks Polakovs <marks@markspolakovs.me>
2022-04-10 08:39:00 +0100

    GRAPHICS-21: Implement skeletal netball graphics

---
+++ bundle-src/src/dashboard/events/netball.tsx
@@ -0,0 +1,42 @@
+import { useOnlyReplicantValue, useReplicantValue } from "common/useReplicant";
+import produce from "immer";
+
+import type { ValueType } from "@ystv/scores/src/common/sports/netball";
+import { ControlNetball } from "common/types/control:netball";
+import { Container, Title } from "@mantine/core";
+import { LiveKillButtons } from "../components/liveKill";
+
+export function NetballDashboard() {
+  const state = useOnlyReplicantValue<ValueType>("eventState");
+  const [control, setControl] = useReplicantValue<ControlNetball>(
+    "control:netball",
+    undefined,
+    {
+      defaultValue: {
+        smallScore: {
+          visible: false,
+        },
+      },
+    }
+  );
+  if (!state) {
+    return <b>No state...?!</b>;
+  }
+
+  return (
+    <Container fluid>
+      <Title order={3}>Quarter {state.quarters.length}</Title>
+      <LiveKillButtons
+        name="Small Score"
+        live={control.smallScore.visible}
+        callback={(live) =>
+          setControl(
+            produce(control, (val) => {
+              val.smallScore.visible = live;
+            })
+          )
+        }
+      />
+    </Container>
+  );
+}
--- bundle-src/src/graphics/index.graphics.tsx
+++ bundle-src/src/graphics/index.graphics.tsx
@@ -1,9 +1,12 @@
 import ReactDOM from "react-dom";
 import React from "react";
+import { AllNetballGraphics } from "./netball";
 import { BaseEventType } from "@ystv/scores/src/common/types";
 import { useOnlyReplicantValue } from "common/useReplicant";
 
-const SportGraphics: Record<string, React.ComponentType> = {};
+const SportGraphics: Record<string, React.ComponentType> = {
+  netball: AllNetballGraphics,
+};
 
 function AllGraphics() {
   const state = useOnlyReplicantValue<BaseEventType>("eventState");
