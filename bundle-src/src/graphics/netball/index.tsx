7e1f46446a015e357aabdffb036ab4980f8a953b
Marks Polakovs <marks@markspolakovs.me>
2022-04-12 19:39:26 +0100

    GRAPHICS-147: Fix bundle on Windows, and other small mistakes

--- bundle-src/src/graphics/netball/index.tsx
+++ bundle-src/src/graphics/netball/index.tsx
@@ -1,12 +1,12 @@
 import { useOnlyReplicantValue } from "common/useReplicant";
 import type { ValueType } from "@ystv/scores/src/common/sports/netball";
-import { ControlNetball } from "common/types/control:netball";
+import { ControlNetball } from "common/types/control-netball";
 import { GraphicContainer } from "../common/container";
 import { NetballSmallScore } from "./SmallScore";
 
 export function AllNetballGraphics() {
   const state = useOnlyReplicantValue<ValueType>("eventState");
-  const control = useOnlyReplicantValue<ControlNetball>("control:netball");
+  const control = useOnlyReplicantValue<ControlNetball>("control-netball");
 
   if (!state || !control) {
     return null;
