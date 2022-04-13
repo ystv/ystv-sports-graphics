cd7fe054e7d3406eb3d6d3085b9246d931c0060f
Marks Polakovs <marks@markspolakovs.me>
2022-04-13 08:25:49 +0100

    GRAPHICS-149: Enable React linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
   },
   extends: [
     "eslint:recommended",
+    "plugin:react/recommended",
     "plugin:react/jsx-runtime",
     "plugin:@typescript-eslint/recommended",
     "prettier",
