68458e21103b3ee02a3d6ab5401e41e9165ccbda
Marks Polakovs <marks@markspolakovs.me>
2022-04-26 07:47:03 +0100

    GRAPHICS-52, GRAPHICS-56: American Football data

---
+++ scores-src/src/common/sports/americanFootball/index.tsx
@@ -0,0 +1,18 @@
+import { createGenericSport } from "../_generic";
+
+const TWELVE_MINUTES = 12 * 60 * 1000;
+
+const { components, typeInfo } = createGenericSport(
+  "americanFootball",
+  [1, 2, 3, 6],
+  [
+    TWELVE_MINUTES,
+    TWELVE_MINUTES,
+    TWELVE_MINUTES,
+    TWELVE_MINUTES,
+    10 * 60 * 1000,
+  ],
+  (i) => (i <= 4 ? "quarter" : "extra time period")
+);
+
+export { components, typeInfo };
