9376f2da7f62a694608c8cf858251b40fdebc407
Marks Polakovs <marks@markspolakovs.me>
2022-04-28 16:27:26 +0100

    GRAPHICS-40: Rugby Union match status

--- bundle-src/src/graphics/rugby/index.tsx
+++ bundle-src/src/graphics/rugby/index.tsx
@@ -5,6 +5,7 @@ import { GraphicContainer } from "../common/container";
 import { Scoreboard } from "./Scoreboard/Scoreboard";
 import { clockTimeAt } from "@ystv/scores/src/common/clock";
 import { useEffect, useRef } from "react";
+import { MatchStatus } from "./MatchStatus/MatchStatus";
 
 export function AllRugbyUnionGraphics() {
   const state = useOnlyReplicantValue<State>("eventState");
@@ -29,6 +30,14 @@ export function AllRugbyUnionGraphics() {
           team1Score={state.scoreHome}
           team2Score={state.scoreAway}
           timer={clockTimeAt(state.clock, new Date().valueOf())}
+        />
+        <MatchStatus
+          isVisible={control.matchStatusPopup.visible}
+          team1Name="LANC"
+          team2Name="YORK"
+          team1Score={state.scoreHome}
+          team2Score={state.scoreAway}
+          isOver={state.halves.length === 2}
         />
       </GraphicContainer>
     </>
