9b94db59aca9c72b2c3fae458604990548d92425
Marks Polakovs <marks@markspolakovs.me>
2022-04-28 16:19:19 +0100

    GRAPHICS-79: Futsal graphics

---
+++ bundle-src/src/dashboard/events/futsal.tsx
@@ -0,0 +1,61 @@
+import { useOnlyReplicantValue, useReplicantValue } from "common/useReplicant";
+import produce from "immer";
+
+import type { State } from "@ystv/scores/src/common/sports/futsal";
+import { ControlFutsal } from "common/types/control-futsal";
+import { Container, Title } from "@mantine/core";
+import { LiveKillButtons } from "../components/liveKill";
+
+export function FutsalDashboard() {
+  const state = useOnlyReplicantValue<State>("eventState");
+  const [control, setControl] =
+    useReplicantValue<ControlFutsal>("control-futsal");
+  console.log(control);
+  if (!state) {
+    return <b>No state...?!</b>;
+  }
+  if (!control) {
+    return <b>No control...?!</b>;
+  }
+
+  return (
+    <Container fluid>
+      <Title order={3}>Half {state.segment}</Title>
+      <LiveKillButtons
+        name="Scoreboard"
+        live={control.scoreboard.visible}
+        callback={(live) =>
+          setControl(
+            produce(control, (val) => {
+              val.scoreboard.visible = live;
+            })
+          )
+        }
+      />
+      <LiveKillButtons
+        divider
+        name="Clock"
+        live={control.scoreboard.showTime}
+        disabled={!control.scoreboard.visible}
+        callback={(live) =>
+          setControl(
+            produce(control, (val) => {
+              val.scoreboard.showTime = live;
+            })
+          )
+        }
+      />
+      <LiveKillButtons
+        name="Match Status Popup"
+        live={control.matchStatusPopup.visible}
+        callback={(live) =>
+          setControl(
+            produce(control, (val) => {
+              val.matchStatusPopup.visible = live;
+            })
+          )
+        }
+      />
+    </Container>
+  );
+}
---
+++ bundle-src/src/graphics/futsal/index.tsx
@@ -0,0 +1,66 @@
+import { useOnlyReplicantValue } from "common/useReplicant";
+import type { State } from "@ystv/scores/src/common/sports/futsal";
+import { ControlFutsal } from "../../common/types/control-futsal";
+import { GraphicContainer } from "../common/container";
+import { Scoreboard } from "../common/scoreboard";
+import { clockTimeAt, formatMMSSMS } from "@ystv/scores/src/common/clock";
+import { useTime } from "../hooks";
+import { MatchStatusPopup } from "../common/matchStatusPopup";
+
+function bannerMsg(half: number) {
+  switch (half) {
+    case 1:
+      return "HALF TIME";
+    case 2:
+      return "FULL TIME";
+    case 3:
+      return "FIRST HALF OF EXTRA TIME";
+    case 4:
+      return "EXTRA TIME";
+  }
+}
+
+export function AllFutsalGraphics() {
+  const state = useOnlyReplicantValue<State>("eventState");
+  const control = useOnlyReplicantValue<ControlFutsal>("control-futsal");
+
+  const now = useTime();
+
+  if (!state || !control) {
+    return null;
+  }
+
+  return (
+    <>
+      {control.scoreboard.visible && (
+        <GraphicContainer>
+          <Scoreboard
+            homeName="LANC"
+            homePrimaryColor="var(--lancaster-red)"
+            homeScore={state.scoreHome}
+            awayName="YORK"
+            awayPrimaryColor="var(--york-white)"
+            awaySecondaryColor="var(--ystv-dark)"
+            awayScore={state.scoreAway}
+            time={formatMMSSMS(clockTimeAt(state.clock, now), 0, 2)}
+            timeVisible={control.scoreboard.showTime}
+          />
+        </GraphicContainer>
+      )}
+      {control.matchStatusPopup.visible && (
+        <GraphicContainer>
+          <MatchStatusPopup
+            homeName="LANC"
+            homePrimaryColor="var(--lancaster-red)"
+            homeScore={state.scoreHome}
+            awayName="YORK"
+            awayPrimaryColor="var(--york-white)"
+            awaySecondaryColor="var(--ystv-dark)"
+            awayScore={state.scoreAway}
+            banner={bannerMsg(state.segment) ?? ""}
+          />
+        </GraphicContainer>
+      )}
+    </>
+  );
+}
