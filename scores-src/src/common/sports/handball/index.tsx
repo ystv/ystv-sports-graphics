3acc2256b91a136c73c87486c44726579887e349
Marks Polakovs <marks@markspolakovs.me>
2022-04-29 09:44:12 +0100

    Fix handball time

--- scores-src/src/common/sports/handball/index.tsx
+++ scores-src/src/common/sports/handball/index.tsx
@@ -3,7 +3,7 @@ import { createGenericSport } from "../_generic";
 const { components, typeInfo } = createGenericSport(
   "handball",
   [1],
-  [25 * 60 * 1000, 5 * 60 * 1000],
+  [30 * 60 * 1000, 5 * 60 * 1000],
   (i) => (i === 1 ? "match" : "extra time period"),
   false,
   true
