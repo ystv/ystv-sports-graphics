40d34af48e103a26907570288c9c010ff64882f6
Marks Polakovs <marks@markspolakovs.me>
2022-04-29 07:58:31 +0100

    handball lineup

--- bundle-src/src/graphics/handball/index.tsx
+++ bundle-src/src/graphics/handball/index.tsx
@@ -6,6 +6,7 @@ import { Scoreboard } from "../common/scoreboard";
 import { clockTimeAt, formatMMSSMS } from "@ystv/scores/src/common/clock";
 import { useTime } from "../hooks";
 import { MatchStatusPopup } from "../common/matchStatusPopup";
+import { LineupTable } from "../common/lineupTable";
 
 function bannerMsg(half: number) {
   if (half === 1) {
@@ -54,6 +55,22 @@ export function AllHandballGraphics() {
             banner={bannerMsg(state.segment) ?? ""}
           />
         )}
+      </GraphicContainer>
+      <GraphicContainer>
+        <LineupTable
+          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+          home={state.players!.home.filter(
+            (x) => x.startsOnBench === control.lineup.substitutes
+          )}
+          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+          away={state.players!.away.filter(
+            (x) => x.startsOnBench === control.lineup.substitutes
+          )}
+          title={
+            state.name + (control.lineup.substitutes ? " - Substitutes" : "")
+          }
+          show={control.lineup.visible}
+        />
       </GraphicContainer>
     </>
   );
