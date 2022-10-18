311545492509556eec584e4a6d40e14581f4df0c
Marks Polakovs <marks@markspolakovs.me>
2022-10-18 20:22:19 +0100

    Make league required (#90)

--- scores-src/src/client/pages/CreateEventModal.tsx
+++ scores-src/src/client/pages/CreateEventModal.tsx
@@ -76,6 +76,7 @@ export function CreateEventModal() {
               // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
               values={leagues.data?.map((l) => [l.slug!, l.name]) ?? []}
               rootAttrs={{ "data-cy": "selectLeague" }}
+              required
             />
             <DateField
               name="startTime"
